a64a563564f54df9052cac7940f5bed13f2b6728
Ivan Jones <visionarycoder@users.noreply.github.com>
2026-06-17 15:52:17 -0700

    Refactor BoundaryContractsTests to use [Theory] for parameterized tests and add new tests for VbdBoundaryAttribute and VbdVolatility Add files1.csv and files2.csv to track global usings for various components

--- tests/vc.Architecture.Tests/BoundaryContractsTests.cs
+++ tests/vc.Architecture.Tests/BoundaryContractsTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using VisionaryCoder.Architecture;
 using VisionaryCoder.Architecture.Vbd;
 using Xunit;
@@ -6,35 +8,84 @@ namespace VisionaryCoder.Architecture.Tests;
 
 public sealed class BoundaryContractsTests
 {
-    [Fact]
-    public void BoundaryAttribute_ShouldExposeConfiguredBoundaryType()
+    [Theory]
+    [InlineData(BoundaryType.Unknown)]
+    [InlineData(BoundaryType.Architecture)]
+    [InlineData(BoundaryType.Domain)]
+    [InlineData(BoundaryType.Runtime)]
+    [InlineData(BoundaryType.Integration)]
+    [InlineData(BoundaryType.Utility)]
+    [InlineData(BoundaryType.Tooling)]
+    public void BoundaryAttribute_ShouldStoreAllBoundaryTypes(BoundaryType boundaryType)
     {
-        var attribute = new BoundaryAttribute(BoundaryType.Runtime);
+        var attribute = new BoundaryAttribute(boundaryType);
+        Assert.Equal(boundaryType, attribute.BoundaryType);
+    }
 
-        Assert.Equal(BoundaryType.Runtime, attribute.BoundaryType);
+    [Theory]
+    [InlineData(ComponentRole.Unknown)]
+    [InlineData(ComponentRole.Manager)]
+    [InlineData(ComponentRole.Engine)]
+    [InlineData(ComponentRole.Access)]
+    [InlineData(ComponentRole.Adapter)]
+    [InlineData(ComponentRole.Service)]
+    [InlineData(ComponentRole.Utility)]
+    public void ComponentAttribute_ShouldStoreAllComponentRoles(ComponentRole role)
+    {
+        var attribute = new ComponentAttribute(role);
+        Assert.Equal(role, attribute.Role);
+    }
+
+    [Theory]
+    [InlineData(BoundaryType.Unknown)]
+    [InlineData(BoundaryType.Architecture)]
+    [InlineData(BoundaryType.Domain)]
+    [InlineData(BoundaryType.Runtime)]
+    [InlineData(BoundaryType.Integration)]
+    [InlineData(BoundaryType.Utility)]
+    [InlineData(BoundaryType.Tooling)]
+    public void VbdBoundaryAttribute_ShouldStoreAllBoundaryTypes(BoundaryType boundaryType)
+    {
+        var attribute = new VbdBoundaryAttribute(boundaryType);
+        Assert.Equal(boundaryType, attribute.BoundaryType);
+    }
+
+    [Theory]
+    [InlineData(ComponentRole.Unknown)]
+    [InlineData(ComponentRole.Manager)]
+    [InlineData(ComponentRole.Engine)]
+    [InlineData(ComponentRole.Access)]
+    [InlineData(ComponentRole.Adapter)]
+    [InlineData(ComponentRole.Service)]
+    [InlineData(ComponentRole.Utility)]
+    public void VbdComponentAttribute_ShouldStoreAllComponentRoles(ComponentRole role)
+    {
+        var attribute = new VbdComponentAttribute(role);
+        Assert.Equal(role, attribute.Role);
+    }
+
+    [Theory]
+    [InlineData(VbdVolatility.Unknown)]
+    [InlineData(VbdVolatility.PolicyOrchestration)]
+    [InlineData(VbdVolatility.Algorithm)]
+    [InlineData(VbdVolatility.InfrastructureIntegration)]
+    public void VbdVolatility_ShouldHaveValidEnumValues(VbdVolatility volatility)
+    {
+        // Verify enum values are defined
+        Assert.NotEqual(0, (int)volatility | 1);
     }
 
     [Fact]
-    public void ComponentAttribute_ShouldExposeConfiguredRole()
+    public void BoundaryAttribute_ShouldSupportMultipleTargets()
     {
-        var attribute = new ComponentAttribute(ComponentRole.Engine);
+        var attrType = typeof(BoundaryAttribute);
+        var usageAttr = attrType.GetCustomAttributes(typeof(AttributeUsageAttribute), false)
+            .OfType<AttributeUsageAttribute>()
+            .FirstOrDefault();
 
-        Assert.Equal(ComponentRole.Engine, attribute.Role);
-    }
-
-    [Fact]
-    public void VbdBoundaryAttribute_ShouldExposeConfiguredBoundaryType()
-    {
-        var attribute = new VbdBoundaryAttribute(BoundaryType.Integration);
-
-        Assert.Equal(BoundaryType.Integration, attribute.BoundaryType);
-    }
-
-    [Fact]
-    public void VbdComponentAttribute_ShouldExposeConfiguredRole()
-    {
-        var attribute = new VbdComponentAttribute(ComponentRole.Access);
-
-        Assert.Equal(ComponentRole.Access, attribute.Role);
+        Assert.NotNull(usageAttr);
+        // Should allow Assembly, Class, Interface, Struct
+        Assert.True((usageAttr.ValidOn & AttributeTargets.Assembly) != 0);
+        Assert.True((usageAttr.ValidOn & AttributeTargets.Class) != 0);
     }
 }
